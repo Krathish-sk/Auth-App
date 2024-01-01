02205f38e766c55487a252608823f457921b0160
Krathish sk <krathishs.k123@gmail.com>
2024-01-01 16:45:08 +0530

    App Intial setup

---
+++ screens/index.js
@@ -0,0 +1,5 @@
+import LoginScreen from "./LoginScreen";
+import SignupScreen from "./SignupScreen";
+import WelcomeSecreen from "./WelcomeSecreen";
+
+export { LoginScreen, SignupScreen, WelcomeSecreen };
