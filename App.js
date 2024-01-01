d9adf66f02187239384ac5256ebda25768073035
Krathish sk <krathishs.k123@gmail.com>
2024-01-01 16:59:36 +0530

    Navigation setup

--- App.js
+++ App.js
@@ -1,30 +1,57 @@
-import { StatusBar } from "expo-status-bar";
 import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import { LoginScreen, SignupScreen, WelcomeSecreen } from "./screens";
 import { COLORS } from "./constants/theme";
 
 const Stack = createNativeStackNavigator();
 
+const AuthStack = function () {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        contentStyle: { backgroundColor: COLORS.primaryBlackHex },
+        headerStyle: { backgroundColor: COLORS.primaryGreyHex },
+        headerTintColor: COLORS.primaryWhiteHex,
+      }}
+    >
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Signup" component={SignupScreen} />
+    </Stack.Navigator>
+  );
+};
+
+const AuthenticatedStack = function () {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: COLORS.primaryGreyHex },
+        headerTintColor: COLORS.primaryWhiteHex,
+        contentStyle: { backgroundColor: COLORS.primaryBlackHex },
+      }}
+    >
+      <Stack.Screen name="Welcome" component={WelcomeSecreen} />
+    </Stack.Navigator>
+  );
+};
+
 function Navigation() {
-  return <NavigationContainer></NavigationContainer>;
+  return (
+    <NavigationContainer>
+      <AuthenticatedStack />
+    </NavigationContainer>
+  );
 }
 
 export default function App() {
   return (
     <>
-      <StatusBar style="Light" backgroundColor={COLORS.primaryBlackHex} />
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor={COLORS.primaryBlackHex}
+      />
       <Navigation />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
